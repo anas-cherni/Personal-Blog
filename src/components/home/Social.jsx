07ad143baf628fbbda22ca5c9a8106d4923a3af9
anascherni <cherniii.anas@gmail.com>
2022-09-03 02:12:13 +0100

    init

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div>
+      <div className="home__social">
+          <a href="https://github.com/anas-cherni" className="home__social-icon" target="_blank">
+              <i className="uil uil-github-alt"></i>
+          </a>
+          <a href="" className="home__social-icon" target="_blank">
+              <i className="uil uil-envelope-alt"></i>
+          </a>
+          <a href="https://twitter.com/chN0S" className="home__social-icon" target="_blank">
+              <i className="uil uil-twitter"></i>
+          </a>
+          <a href="" className="home__social-icon" target="_blank">
+              <i className="uil uil-document-layout-left"></i>
+          </a>
+
+      </div>
+    </div>
+  )
+}
+
+export default Social
